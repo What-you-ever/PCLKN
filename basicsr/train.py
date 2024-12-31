c0ec34afcd8fe26465ebdc61494b5b827915a2ff
What-you-ever <77673770+What-you-ever@users.noreply.github.com>
2024-12-31 15:14:43 +0800

    Update train.py

--- basicsr/train.py
+++ basicsr/train.py
@@ -18,8 +18,7 @@ from basicsr.data.data_sampler import EnlargedSampler
 from basicsr.data.prefetch_dataloader import CPUPrefetcher, CUDAPrefetcher
 from basicsr.models import build_model
 from basicsr.utils import (AvgTimer, MessageLogger, check_resume, get_env_info, get_root_logger, get_time_str,
-                           init_tb_logger, init_wandb_logger, make_exp_dirs, mkdir_and_rename, mkdir_with_timestap,
-                           scandir)
+                           init_tb_logger, init_wandb_logger, make_exp_dirs, mkdir_and_rename, scandir)
 from basicsr.utils.options import copy_opt_file, dict2str, parse_options
 
 
